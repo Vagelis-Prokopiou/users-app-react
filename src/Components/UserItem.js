5d736d68d230cfa329309c19f7722707c5844872
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2018-03-28 09:44:01 +0300

    Making UserItem stateless component
    Signed-off-by: Vagelis Prokopiou <vagelis.prokopiou@gmail.com>

--- src/Components/UserItem.js
+++ src/Components/UserItem.js
@@ -1,20 +1,16 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
-class UserItem extends Component {
-  render() {
-    return (
-        <tr className="User">
-          <td>
-            <Link to={`/user/${this.props.user.id}`}>
-              {this.props.user.name}
-            </Link>
-          </td>
-          <td>{this.props.user.numberOfPosts}</td>
-          <td>{this.props.user.commentsPerPost}</td>
-        </tr>
-    );
-  }
-}
+const UserItem = props => (
+    <tr className="User">
+      <td>
+        <Link to={`/user/${props.user.id}`}>
+          {props.user.name}
+        </Link>
+      </td>
+      <td>{props.user.numberOfPosts}</td>
+      <td>{props.user.commentsPerPost}</td>
+    </tr>
+);
 
 export default UserItem;
